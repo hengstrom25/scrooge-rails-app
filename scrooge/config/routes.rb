8ccea8cefe6ba05b36b4bc1bec3dbd7b365e0260
Heidi <hengstrom25@gmail.com>
2018-10-25 22:44:38 -0400

    add sessions routes, users and static controllers

--- scrooge/config/routes.rb
+++ scrooge/config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'static_pages#home'
+  get '/signin' => 'sessions#new'
+  post '/signin' => 'sessions#create'
+
+  post 'logout', to: 'sessions#destroy'
+  resources :users, only: [:new, :create, :show]
+
+  delete 'logout' => 'sessions#destroy'
 end
