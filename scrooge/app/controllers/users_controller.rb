40a70a1168f8995253a4244ec97ec0ebc3443ce0
Heidi <hengstrom25@gmail.com>
2018-11-10 10:24:44 -0500

    add bootstrap to application.html.erb

--- scrooge/app/controllers/users_controller.rb
+++ scrooge/app/controllers/users_controller.rb
@@ -26,7 +26,7 @@ class UsersController < ApplicationController
 	private
   
 	def user_params
-  		params.require(:user).permit(:name, :password, :admin)
+  		params.require(:user).permit(:name, :email, :user_name, :password)
   	end
   
 end
