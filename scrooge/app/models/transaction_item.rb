12b651c548a719ce321d657b3fe0d4f8b0b57352
Heidi <hengstrom25@gmail.com>
2018-12-11 23:00:15 -0500

    update transaction show view and transaction_item model

--- scrooge/app/models/transaction_item.rb
+++ scrooge/app/models/transaction_item.rb
@@ -1,5 +1,5 @@
 class TransactionItem < ActiveRecord::Base
-	belongs_to :trans, :class_name => 'Transaction'
+	belongs_to :trans, :class_name => 'Transaction', :foreign_key => 'transaction_id' 
 	belongs_to :item
 
 end
